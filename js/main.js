2045434ab636b612ba007320ae30390bb46b2e6d
¨Efren <efrenbarcevr@gmail.com>
2019-06-12 22:24:57 -0500

    flexslider first fixed

--- js/main.js
+++ js/main.js
@@ -149,19 +149,7 @@
 			animation: "fade",
 			slideshowSpeed: 5000,
 			directionNav: true,
-			start: function(){
-				setTimeout(function(){
-					$('.slider-text').removeClass('animated fadeInUp');
-					$('.flex-active-slide').find('.slider-text').addClass('animated fadeInUp');
-				}, 500);
-			},
-			before: function(){
-				setTimeout(function(){
-					$('.slider-text').removeClass('animated fadeInUp');
-					$('.flex-active-slide').find('.slider-text').addClass('animated fadeInUp');
-				}, 500);
-			}
-
+			
 	  	});
 
 	};
