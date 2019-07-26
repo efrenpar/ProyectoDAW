52e7c6b04cfac51f549bf4c9df714d6935573ce8
davnm5 <josdanei@espol.edu.ec>
2019-07-26 14:12:33 -0500

    [ADD] Data

--- js/search.js
+++ js/search.js
@@ -4,61 +4,8 @@ var app = new Vue({
   data: {
     filterKey: 'todos',
     search: '',
-    artistas: [
-      {
-        autor: 'Anibal Villacis', nacimiento: '1927; Ambato, Ecuador', nacionalidad: 'Ecuatoriana',
-        movimiento: 'Informalismo precolombino - indigenista', campo: 'Pintura', img: 'images/AnibalVillacis.jpg'
-      },
-      {
-        autor: 'Enrique Tábara', nacimiento: '1930; Guayaquil, Ecuador', nacionalidad: 'Ecuatoriana',
-        movimiento: 'Expresionista, Abstraccion, Construtivista precolombino', campo: 'Pintura', img: 'images/EnriqueTabara.jpeg'
-      },
-      {
-        autor: 'Bolivar Peñafiel', nacimiento: '1934; Guayaquil, Ecuador', nacionalidad: 'Ecuatoriana',
-        movimiento: 'Neofigurativismo', campo: 'Pintura', img: 'images/LuisPena.jpg'
-      },
-      {
-        autor: 'Eduardo Kingman', nacimiento: '1913; Loja, Ecuador', nacionalidad: 'Ecuatoriana',
-        movimiento: 'Figurativismo, Realismo Social', campo: 'Pintura', img: 'images/kingman.jpg'
-      },
-      {
-        autor: 'Hector Ramirez', nacimiento: '1982;Guayaquil, Ecuador', nacionalidad: 'Ecuatoriana',
-        movimiento: 'Abstraccionismo', campo: 'Pintura', img: 'images/ramirez.jpg'
-      },
-      {
-        autor: 'Antonio Arias', nacimiento: '1944; Quito, Ecuador', nacionalidad: 'Ecuatoriana',
-        movimiento: 'Neofigurativismo', campo: 'Pintura,Escultura', img: 'images/arias.jpg'
-      },
-      {
-        autor: 'Carlos Catasse', nacimiento: '1944; Santiago de Chile, Chile', nacionalidad: 'Chilena',
-        movimiento: 'Neofigurativo - Abstracto', campo: 'Pintura', img: 'images/catasse.jpg'
-      },
-      {
-        autor: 'Manuel Velastegui', nacimiento: '1944; Guayaquil, Ecuador', nacionalidad: 'Ecuatoriana',
-        movimiento: 'Neofigurativo', campo: 'Escultura', img: 'images/velastegui.jpg'
-      },
-
-
-    ],
-    obras: [{
-      autor: "Anibal Villacis", obras: ["AV-00109.jpg", "AV-00109.jpg", "AV-00114.jpg", "AV-00119.jpg", "AV-00119.jpg", "AV-00127.jpg", "AV-00137.jpg", "AV-00139.jpg"]
-    }, {
-      autor: "Enrique Tábara", obras: ["ET-00101.jpg", "ET-00104.jpg", "ET-00160.jpg", "ET-00163.jpg"]
-
-    }, {
-      autor: "Bolivar Peñafiel", obras: ["BPM-00104.jpg", "BPM-00128.jpg", "BPM-00165.jpg", "BPM-00196.jpg", "BPM-00208.jpg", "BPM-00310.jpg", "BPM-00373.jpg"]
-
-    }, {
-      autor: "Eduardo Kingman", obras: ["LPB-00109.jpg", "LPB-00129.jpg", "LPB-00147.jpg", "LPB-00156.jpg", "LPB-00218.jpg", "LPB-00222.jpg", "LPB-00259.jpg", "LPB-00231.jpg"]
-
-    }, {
-      autor: "Carlos Catasse", obras: ["HRAM-00105.jpg", "HRAM-00164.jpg", "HRAM-00177.jpg", "HRAM-00178.jpg", "HRAM-00186.jpg", "HRAM-00199.jpg", "HRAM-00209.jpg", "HRAM-00226.jpg"]
-
-    }, {
-      autor: "Manuel Velastegui", obras: ["HULA-00121.jpg", "HULA-00123.jpg", "HULA-00124.jpg", "HULA-00130.jpg", "HULA-00141.jpg", "HULA-00146.jpg"]
-
-    },
-    ],
+    artistas: null,
+    obras: null,
   },
   computed: {
     filtroArtistas() {
@@ -104,7 +51,7 @@ function mostrar_obras(autor) {
 
   for (var i = 0; i < aux.length; i++) {
     if (aux[i].autor == autor) {
-      var imagenes = aux[i].obras;
+      var imagenes = aux[i].imagenes;
       console.log(imagenes);
       for (var j = 0; j < imagenes.length; j++) {
         var img = document.createElement('img');
@@ -124,4 +71,15 @@ function mostrar_obras(autor) {
   });
   $('.error').removeClass('alert alert-danger').html('');
 }
-
+/*
+$.getJSON('https://project-daw-gallery.herokuapp.com/data/artistas',function(json) {
+    var aux = json.escritores;
+    console.log(aux);
+    app.artistas=aux;
+});
+*/
+$.getJSON('https://project-daw-gallery.herokuapp.com/data/obras',function(json) {
+    var aux = json.obras;
+    console.log(aux);
+    app.obras=aux;
+});
