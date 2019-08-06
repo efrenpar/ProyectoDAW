c26c42e9ae01aa4f4c5d1e9053a40c822e4427f1
davnm5 <josdanei@espol.edu.ec>
2019-08-05 23:21:37 -0500

    [ADD] Servidor Express

--- app.js
+++ app.js
@@ -57,5 +57,9 @@ app.use(function(req, res, next) {
   
   });
 
-app.listen(process.env.port || 5000);
-console.log("running at port 5000");
+const host = '0.0.0.0';
+const port = process.env.PORT || 3000;  
+  
+app.listen(port, host, function() {
+  console.log("Server started.......");
+});
